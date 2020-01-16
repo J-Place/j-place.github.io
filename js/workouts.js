4f158b0d5f3879f52874758e6c2440f14558c4c0
J-Place <jay@cooper-james.comjplace@usmastersswimming.org>
2020-01-16 17:45:16 -0500

    "

--- js/workouts.js
+++ js/workouts.js
@@ -3,17 +3,13 @@ function getWorkouts() {
 	const xhr = new XMLHttpRequest();
   
 	xhr.open('GET', '/KD Great Workout.json?ver=1', true);
-	// xhr.open('GET', '/swimFeedJP.json?asd=1', true);
 	xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
-	// xhr.withCredentials = true;
 	xhr.onload = function () {
 	  if (xhr.status === 200) {
-		// hideLoadingOverlay();
 		const response = JSON.parse(xhr.response);
-		// allSwims = response.swims;
-		// const stripped = { ...response, swims: response.swims.slice(0, (perPage - 1) * currentPage++) };
-		// createSwimsHtml(stripped);
-		console.log(response);
+    // createWorkoutsHtml(response);
+    data = response.data;
+		console.log(data);
 		return;
 	  }
 	  console.log("Error retrieving swim information");
