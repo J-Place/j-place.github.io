90c3de6582d1f4f1c515b2383e2ab9c5c735a615
J-Place <jay@cooper-james.comjplace@usmastersswimming.org>
2020-01-19 12:55:54 -0500

    general set up for header, pages, etc.

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,5 +1,22 @@
-import './home.scss';
-import Rotator from '../../js/Rotator';
-
-const rotator = new Rotator();
-rotator.rotate(document.getElementsByClassName('logo')[0], 500);
+function getLatestSwims() {
+    showLoadingOverlay();
+    const xhr = new XMLHttpRequest();
+  
+    xhr.open('GET', '/swimFeedJP.json?ver=1', true);
+    // xhr.open('GET', '/swimFeedJP.json?asd=1', true);
+    xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
+    // xhr.withCredentials = true;
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        hideLoadingOverlay();
+        const response = JSON.parse(xhr.response);
+        allSwims = response.swims;
+        const stripped = { ...response, swims: response.swims.slice(0, (perPage - 1) * currentPage++) };
+        createSwimsHtml(stripped);
+        return;
+      }
+      console.log("Error retrieving swim information");
+      return null;
+    };
+    xhr.send();
+  }
