acc840c135cfba6505103bdf81c574083d1a5e4a
J-Place <jay@cooper-james.comjplace@usmastersswimming.org>
2020-01-19 15:00:10 -0500

    updates

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,22 +1,34 @@
-function getLatestSwims() {
-    showLoadingOverlay();
+function getWorkoutData() {
     const xhr = new XMLHttpRequest();
   
-    xhr.open('GET', '/swimFeedJP.json?ver=1', true);
-    // xhr.open('GET', '/swimFeedJP.json?asd=1', true);
+    // xhr.open('GET', '/swimFeedJP.json?ver=1', true);
+    xhr.open('GET', '/KD Great Workout.json', true);
     xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
-    // xhr.withCredentials = true;
     xhr.onload = function () {
       if (xhr.status === 200) {
-        hideLoadingOverlay();
         const response = JSON.parse(xhr.response);
-        allSwims = response.swims;
-        const stripped = { ...response, swims: response.swims.slice(0, (perPage - 1) * currentPage++) };
-        createSwimsHtml(stripped);
+        var workoutData = response;
+        renderWorkouts(workoutData);       
         return;
       }
       console.log("Error retrieving swim information");
       return null;
     };
     xhr.send();
+  }
+  getWorkoutData();
+
+
+//   var workoutData = require('./KD Great Workout.json');
+
+//   renderWorkouts(workoutData);
+
+  // Populate Workout Results Template
+function renderWorkouts(workout) {
+    console.log(workout.data.workouttitle);
+    var rawTemplate = document.getElementById("workoutResultsTemplate").innerHTML;
+    var compiledTemplate = Handlebars.compile(rawTemplate);
+    var ourGeneratedHTML = compiledTemplate(workout)
+    var wrapper = document.getElementById("workoutResultsWrapper");
+    wrapper.innerHTML = ourGeneratedHTML;
   }
