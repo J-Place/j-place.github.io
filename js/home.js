e7c430f461f4e5331ea6718a5b32640954c13d5d
J-Place <jplace@usmastersswimming.org>
2020-11-17 13:20:47 -0500

    remove workout finder from main content

--- js/home.js
+++ js/home.js
@@ -46,39 +46,39 @@
 
 	'use strict';
 
-	function getWorkoutData() {
-	  var xhr = new XMLHttpRequest();
+	// function getWorkoutData() {
+	//   var xhr = new XMLHttpRequest();
 
-	  // xhr.open('GET', '/swimFeedJP.json?ver=1', true);
-	  xhr.open('GET', '/KD Great Workout.json', true);
-	  xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
-	  xhr.onload = function () {
-	    if (xhr.status === 200) {
-	      var response = JSON.parse(xhr.response);
-	      var workoutData = response;
-	      renderWorkouts(workoutData);
-	      return;
-	    }
-	    console.log("Error retrieving swim information");
-	    return null;
-	  };
-	  xhr.send();
-	}
-	getWorkoutData();
+	//   // xhr.open('GET', '/swimFeedJP.json?ver=1', true);
+	//   xhr.open('GET', '/KD Great Workout.json', true);
+	//   xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
+	//   xhr.onload = function () {
+	//     if (xhr.status === 200) {
+	//       var response = JSON.parse(xhr.response);
+	//       var workoutData = response;
+	//       renderWorkouts(workoutData);
+	//       return;
+	//     }
+	//     console.log("Error retrieving swim information");
+	//     return null;
+	//   };
+	//   xhr.send();
+	// }
+	// getWorkoutData();
 
 	//   var workoutData = require('./KD Great Workout.json');
 
 	//   renderWorkouts(workoutData);
 
 	// Populate Workout Results Template
-	function renderWorkouts(workout) {
-	  console.log(workout.data.workouttitle);
-	  var rawTemplate = document.getElementById("workoutResultsTemplate").innerHTML;
-	  var compiledTemplate = Handlebars.compile(rawTemplate);
-	  var ourGeneratedHTML = compiledTemplate(workout);
-	  var wrapper = document.getElementById("workoutResultsWrapper");
-	  wrapper.innerHTML = ourGeneratedHTML;
-	}
+	// function renderWorkouts(workout) {
+	//   console.log(workout.data.workouttitle);
+	//   var rawTemplate = document.getElementById("workoutResultsTemplate").innerHTML;
+	//   var compiledTemplate = Handlebars.compile(rawTemplate);
+	//   var ourGeneratedHTML = compiledTemplate(workout);
+	//   var wrapper = document.getElementById("workoutResultsWrapper");
+	//   wrapper.innerHTML = ourGeneratedHTML;
+	// }
 
 /***/ })
 /******/ ]);
