6a06fd6676b5d6d9195b72a59c5481eaa71038c5
J-Place <jplace@usmastersswimming.org>
2020-11-19 15:16:03 -0500

    "

--- js/results.js
+++ js/results.js
@@ -7,6 +7,7 @@ function getEventResults() {
     xhr.onload = function () {
         if (xhr.status === 200) {
             const response = JSON.parse(xhr.response);
+            console.log(response[0].name);
             createHtml(response);
         return;
         }
@@ -19,8 +20,8 @@ getEventResults();
 
 
 function createHtml() {
-    console.log(response);
-    console.log(response[0].name);
+    // console.log(response[0].name);
+    console.log('create html function');
 }
 createHtml();
 
