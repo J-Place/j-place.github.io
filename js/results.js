ba7a8cb02a8fbb7d33e2ca61ee84782d67a823ba
J-Place <jplace@usmastersswimming.org>
2020-11-19 14:18:52 -0500

    add log

--- js/results.js
+++ js/results.js
@@ -1,5 +1,5 @@
 function getEventResults() {
-    // showLoadingOverlay();
+
     const xhr = new XMLHttpRequest();
   
     xhr.open('GET', 'https://763a048a-2a06-44c5-a12f-f004327581ab.mock.pstmn.io/get', true);
@@ -8,6 +8,7 @@ function getEventResults() {
         if (xhr.status === 200) {
             const response = JSON.parse(xhr.response);
             console.log(response);
+            createHtml(response);
         return;
     }
         return null;
@@ -17,43 +18,48 @@ function getEventResults() {
 getEventResults();
 
 
-/* List.js is required to make this table work. */
 
-var options = {
-    valueNames: [ { data: ['timestamp'] }, { data: ['status'] }, 'jSortNumber', 'jSortName', 'jSortTotal' ],
-    page: 2,
-    pagination: {
-      innerWindow: 1,
-      left: 0,
-      right: 0,
-      paginationClass: "pagination",
-      }
-  };
-  
-  var tableList = new List('tableID', options);
-  
-  
-      $('.jPaginateNext').on('click', function(){
-          var list = $('.pagination').find('li');
-          $.each(list, function(position, element){
-              if($(element).is('.active')){
-                  $(list[position+1]).trigger('click');
-              }
-          })
-      });
-  
-  
-      $('.jPaginateBack').on('click', function(){
-          var list = $('.pagination').find('li');
-          $.each(list, function(position, element){
-              if($(element).is('.active')){
-                  $(list[position-1]).trigger('click');
-              }
-          })
-      });
-  
-  
+function createHtml() {
+    console.log(response[0].name);
+}
+createHtml();
 
 
-  
-  
+
+
+
+// /* List.js is required to make this table work. */
+
+// var options = {
+//     valueNames: [ { data: ['timestamp'] }, { data: ['status'] }, 'jSortNumber', 'jSortName', 'jSortTotal' ],
+//     page: 2,
+//     pagination: {
+//       innerWindow: 1,
+//       left: 0,
+//       right: 0,
+//       paginationClass: "pagination",
+//       }
+//   };
+
+
+
+
+//   var tableList = new List('tableID', options);  
+
+//       $('.jPaginateNext').on('click', function(){
+//           var list = $('.pagination').find('li');
+//           $.each(list, function(position, element){
+//               if($(element).is('.active')){
+//                   $(list[position+1]).trigger('click');
+//               }
+//           })
+//       });
+
+//       $('.jPaginateBack').on('click', function(){
+//           var list = $('.pagination').find('li');
+//           $.each(list, function(position, element){
+//               if($(element).is('.active')){
+//                   $(list[position-1]).trigger('click');
+//               }
+//           })
+//       });
