39252a8be3eb10918096fd7d53973dd12e9d211d
J-Place <jplace@usmastersswimming.org>
2020-11-20 10:55:52 -0500

    update

--- js/results.js
+++ js/results.js
@@ -4,101 +4,99 @@ function getEventResults(createHtml) {
   xhr.open('GET', 'https://763a048a-2a06-44c5-a12f-f004327581ab.mock.pstmn.io/get', true);
   xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
   xhr.onload = function () {
-      if (xhr.status === 200) {
-          const data = JSON.parse(xhr.response);
-          createHtml(data);
-      return;
-      }
-      return null;
+    if (xhr.status === 200) {
+        const data = JSON.parse(xhr.response);
+        var options = {
+          page: 100,
+          pagination: true,
+          valueNames: [ 'Name', 'Age', 'Club', 'Total' ]
+        };    
+        var resultsGtd = new List('resultsGtd', options, data);
+        resultsGtd.sort('total', {
+          order:'desc',
+        });
+        // createHtml(data);
+    return;
+    }
+    return null;
   };
   xhr.send();
 }
 
-var options = {
-  page: 6,
-  pagination: true,
-  valueNames: [ 'name', 'age', 'club', 'total' ]
-};
-
-var values = [
-  {
-    name: 'Jay Place',
-    age: 48,
-    club: 'SRQM',
-    total: "13,132"
-  },
-  {
-    name: 'Tom Jones',
-    age: 80,
-    club: 'SHARK',
-    total: "1,000"
-  },
-  {
-    name: 'Daniel Pauling',
-    age: 40,
-    club: 'SHARK',
-    total: "199,999"
-  },
-  {
-    name: 'Bobby Boucher',
-    age: 55,
-    club: 'SHARK',
-    total: '9,000'
-  },
-  {
-    name: 'Tom Brady',
-    age: 43,
-    club: 'SRQM',
-    total: '19,000'
-  },
-  {
-    name: 'Sunny Garcia',
-    age: 49,
-    club: 'SRQM',
-    total: '23,000'
-  },
-  {
-    name: 'Cooper Place',
-    age: 17,
-    club: 'SRQM',
-    total: '6,000'
-  },
-  {
-    name: 'Morgan Place',
-    age: 11,
-    club: 'SRQM',
-    total: '999,999'
-  },
-  {
-    name: 'Dan Mullen',
-    age: 46,
-    club: 'SHARK',
-    total: '12,000'
-  }
-];
-
-var resultsGtd = new List('resultsGtd', options, values );
-
-resultsGtd.sort('total', {
-  order:'desc',
-})
 
 
-function createHtml() { 
-  getEventResults(function(data) {
 
-    // Show 5 Results
-    for (let i = 0; i < 5; i++) {
-      console.log(data[i]);
-    }
+// var values = [
+//   {
+//     name: 'Jay Place',
+//     age: 48,
+//     club: 'SRQM',
+//     total: "13,132"
+//   },
+//   {
+//     name: 'Tom Jones',
+//     age: 80,
+//     club: 'SHARK',
+//     total: "1,000"
+//   },
+//   {
+//     name: 'Daniel Pauling',
+//     age: 40,
+//     club: 'SHARK',
+//     total: "199,999"
+//   },
+//   {
+//     name: 'Bobby Boucher',
+//     age: 55,
+//     club: 'SHARK',
+//     total: '9,000'
+//   },
+//   {
+//     name: 'Tom Brady',
+//     age: 43,
+//     club: 'SRQM',
+//     total: '19,000'
+//   },
+//   {
+//     name: 'Sunny Garcia',
+//     age: 49,
+//     club: 'SRQM',
+//     total: '23,000'
+//   },
+//   {
+//     name: 'Cooper Place',
+//     age: 17,
+//     club: 'SRQM',
+//     total: '6,000'
+//   },
+//   {
+//     name: 'Morgan Place',
+//     age: 11,
+//     club: 'SRQM',
+//     total: '999,999'
+//   },
+//   {
+//     name: 'Dan Mullen',
+//     age: 46,
+//     club: 'SHARK',
+//     total: '12,000'
+//   }
+// ];
 
-    // Show All Results
-    // for (var i in data) {
-      // console.log(data[i]);
-    // }
-    
-  });
+// var resultsGtd = new List('resultsGtd', options, values );
 
-}
-createHtml();
+
+// function createHtml() { 
+//   getEventResults(function(data) {
+//     // Show 5 Results
+//     for (let i = 0; i < 5; i++) {
+//       console.log(data[i]);
+//     }
+//     // Show All Results
+//     // for (var i in data) {
+//       // console.log(data[i]);
+//     // }
+//   });
+// }
+// createHtml();
 
