1cdb6df3ebb9c16f8d53662f1a4c10e04afbc3d6
J-Place <jplace@usmastersswimming.org>
2020-11-23 13:39:31 -0500

    add api data

--- js/results.js
+++ js/results.js
@@ -6,15 +6,6 @@ function getEventResults(createHtml) {
   xhr.onload = function () {
     if (xhr.status === 200) {
         const data = JSON.parse(xhr.response);
-        // var options = {
-        //   page: 2,
-        //   pagination: true,
-        //   valueNames: [ 'Name', 'Age', 'Club', 'Total' ]
-        // };    
-        // var resultsGtd = new List('resultsGtd', options, data);
-        // resultsGtd.sort('total', {
-        //   order:'desc',
-        // });
         createHtml(data);
     return;
     }
@@ -23,82 +14,32 @@ function getEventResults(createHtml) {
   xhr.send();
 }
 
-var options = {
-  page: 6,
-  pagination: true,
-  valueNames: [ 'name', 'age', 'club', 'total' ]
-};
-
-var values = [
-  {
-    name: 'Jay Place',
-    age: 48,
-    club: 'SRQM',
-    total: "13,132"
-  },
-  {
-    name: 'Tom Jones',
-    age: 80,
-    club: 'SHARK',
-    total: "1,000"
-  },
-  {
-    name: 'Daniel Pauling',
-    age: 40,
-    club: 'SHARK',
-    total: "199,999"
-  },
-  {
-    name: 'Bobby Boucher',
-    age: 55,
-    club: 'SHARK',
-    total: '9,000'
-  },
-  {
-    name: 'Tom Brady',
-    age: 43,
-    club: 'SRQM',
-    total: '19,000'
-  },
-  {
-    name: 'Sunny Garcia',
-    age: 49,
-    club: 'SRQM',
-    total: '23,000'
-  },
-  {
-    name: 'Cooper Place',
-    age: 17,
-    club: 'SRQM',
-    total: '6,000'
-  },
-  {
-    name: 'Morgan Place',
-    age: 11,
-    club: 'SRQM',
-    total: '999,999'
-  },
-  {
-    name: 'Dan Mullen',
-    age: 46,
-    club: 'SHARK',
-    total: '12,000'
-  }
-];
-
-var resultsGtd = new List('resultsGtd', options, values );
-
 
 function createHtml() { 
   getEventResults(function(data) {
+
     // Show 5 Results
-    // for (let i = 0; i < 5; i++) {
-    //   console.log(data[i]);
-    // }
-    // Show All Results
-    for (var i in data) {
+    for (let i = 0; i < 5; i++) {
       console.log(data[i]);
     }
+
+    // Show All Results
+    // for (var i in data) {
+    //   console.log(data[i].Miles);
+    // }
+
+    var options = {
+      page: 9,
+      pagination: true,
+      valueNames: [ 'Name', 'Age', 'Club', 'Miles' ]
+    };
+    
+    var resultsGtd = new List('resultsGtd', options, data );
+    
+    resultsGtd.sort('Miles', {
+      order:'desc',
+    });
+
   });
 }
 createHtml();
