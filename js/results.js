b663ad264146ca279236e21450df821bb468e580
J-Place <jplace@usmastersswimming.org>
2020-11-23 14:38:39 -0500

    rename function

--- js/results.js
+++ js/results.js
@@ -1,12 +1,12 @@
 
-function getEventResults(createHtml) {
+function getEventResults(createTable) {
   const xhr = new XMLHttpRequest();  
   xhr.open('GET', 'https://763a048a-2a06-44c5-a12f-f004327581ab.mock.pstmn.io/get', true);
   xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
   xhr.onload = function () {
     if (xhr.status === 200) {
         const data = JSON.parse(xhr.response);
-        createHtml(data);
+        createTable(data);
     return;
     }
     return null;
@@ -15,7 +15,7 @@ function getEventResults(createHtml) {
 }
 
 
-function createHtml() { 
+function createTable() { 
   getEventResults(function(data) {
 
     // Show 5 Results
@@ -42,5 +42,5 @@ function createHtml() {
 
   });
 }
-createHtml();
+createTable();
 
