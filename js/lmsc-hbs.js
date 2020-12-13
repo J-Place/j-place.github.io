bdb9ff24eacbf20f6555ae0b3b5cd182933e25df
J-Place <jplace@usmastersswimming.org>
2020-12-13 10:15:27 -0500

    no message

--- js/lmsc-hbs.js
+++ js/lmsc-hbs.js
@@ -6,7 +6,7 @@ function hideLoadingSpinner() {
 }
 hideLoadingSpinner();  
 
-
+var data = [];
 
 var getParticipants = $.ajax({
     url: "https://j-place.github.io/participants_all.json",
@@ -15,7 +15,7 @@ var getParticipants = $.ajax({
       var data = JSON.parse(getParticipants.responseText);
       createHtml(data);
     //   formatMiles(data);
-    //   formatAverage(data);
+      formatAvg(data);
     //   cleanData(data);
     //   console.log(data.data[0].average.toFixed(2));
     },
@@ -24,16 +24,19 @@ var getParticipants = $.ajax({
     }
 })
 
+console.log(data[0].average);
 
-var lmscGroup = _.groupBy(data, function(lmsc){ 
-    return lmsc.name.substr(0,1); 
-});
+// var lmscGroup = _.groupBy(data, function(lmsc){ 
+//     return lmsc.name.substr(0,1); 
+// });
 
-// function formatAverage(average) {
-//     let formattedAvg = average.toFixed(2);;
-//     average = formattedAvg;  
-//     return average;
-//   }
+function formatAvg(average) {
+    let formattedAvg = average.toFixed(2);;
+    average = formattedAvg;  
+    return average;
+}
+  
+
   
 
 // function formatMiles(miles) {  
@@ -41,16 +44,11 @@ var lmscGroup = _.groupBy(data, function(lmsc){
 //     return miles;
 // }
 
-// function formatAvg(average) {  
-//     average = data.data[0].average.toFixed(2);
-//     return average;
-// }
-
 // function cleanData(data) {
 //     let tempData = data; // Use local data object until api access is fixed
 //     for (var i = 0; i < tempData.length; i++) {
 //         // tempData[i].miles = formatMiles(data[i].miles);
-//         tempData[i].average = formatAverage(data[i].average);
+//         tempData[i].average = formatAvg(data[i].average);
 //     }
 // };
 
