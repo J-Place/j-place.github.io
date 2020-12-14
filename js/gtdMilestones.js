45d38e2cca3fe42bb51b3a79477ea381f8851da5
J-Place <jplace@usmastersswimming.org>
2020-12-14 03:03:06 -0500

    no message

--- js/gtdMilestones.js
+++ js/gtdMilestones.js
@@ -3,177 +3,130 @@
 // }
 
 function hideLoadingSpinner() {
-  const loadingSpinner = document.getElementById('loadingWrapper');
-  loadingSpinner.classList.add('hide');
-}
-hideLoadingSpinner();
+    const loadingSpinner = document.getElementById('loadingWrapper');
+    loadingSpinner.classList.add('hide');
+  }
+  hideLoadingSpinner();
 
-// function getEventResults() {
-//   // showLoadingOverlay();
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://test.usms.org/apis/v1/gtd/participants', true);
-//   xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
-//   xhr.onload = function () {
-//     if (xhr.status === 200) {
-//       const checkApi = JSON.parse(xhr.response);
-//       return;
-//     }
-//     console.log(checkApi);
-//     return null;
-//   };
-//   xhr.send();
-// }
-// getEventResults();
-
-
-// function getEventResults() {
-//   const xhr = new XMLHttpRequest();
-//   // xhr.open('GET', `/apis/v1/gtd/participants`, true);
-//   xhr.open('GET', `https://test.usms.org/apis/v1/gtd/participants`, true);
-//   xhr.withCredentials = true;
-//   xhr.onload = function () {
-//     if (xhr.status === 200) {
-//       const response = JSON.parse(xhr.response);
-//     }
-//     console.log(response);
-//   };
-//   xhr.send();
-// }
-// getEventResults();
-
-
-
-var options = {
-  page: 99,
-  pagination: true,
-  innerWindow: 1,
-  left: 0,
-  right: 0,
-  paginationClass: "pagination",
-  valueNames: [ 'milestone', 'first', 'last', 'age', 'sex', 'clubAbbr', 'dateAchieved' ],
-};
-
-
-var getMilestones = $.ajax({
-    url: "https://j-place.github.io/milestones.json",
-    type: "GET",
-    success: function(response) {
-      var data = JSON.parse(getMilestones.responseText);
-      var newData = data;
-    //   createHtml(newData);
-    //   formatMiles(data);
-    //   formatAvg(newData);
-        // cleanData(data);
-        loadData(newData);
-        // console.log(data.data[0].milestone);
-    },
-    error: function(xhr) {
-      console.log("Failed to load data.");
-    }
-})
-
-function loadData(data) {
-    console.log(data.data[0].milestone);
-    var gtdMilestones = new List('gtdMilestones', options, newData );
-
-    // List sort defaults
-    gtdMilestones.sort(
-        'milestone', {
-        order:'asc',
-        }
-    );
   
-// function formatMiles(miles) {  
-//   miles = miles.toFixed(2);
-//   return miles;
-// }
+  
+  var options = {
+    page: 99,
+    pagination: true,
+    innerWindow: 1,
+    left: 0,
+    right: 0,
+    paginationClass: "pagination",
+    valueNames: [ 'milestone', 'first', 'last', 'age', 'sex', 'clubAbbr', 'dateAchieved' ],
+  };
+
+
+//   var dataLocal = [data];
+  
+  
+  var getMilestones = $.ajax({
+      url: "https://j-place.github.io/milestones.json",
+      type: "GET",
+      success: function(response) {
+        var data = JSON.parse(getMilestones.responseText);
+        console.log(data.data[0]);
+        console.log(data.data[0].milestone);
+        var newData = data.data;
+    //   handleFilters(newData);
+          loadData(data.data);
+      },
+      error: function(xhr) {
+        console.log("Failed to load data.");
+      }
+  })
+  
+    function loadData(data) {
+      console.log(data[0].milestone);
+
+      var gtdMilestones = new List('gtdMilestones', options, data );
+  
+      // List sort defaults
+      gtdMilestones.sort(
+          'milestone', {
+          order:'asc',
+          }
+      );
+    
+  // function formatMiles(miles) {  
+  //   miles = miles.toFixed(2);
+  //   return miles;
+  // }
+
     // function cleanData(data) {
     //     for (var i = 0; i < data.length; i++) {
-    //         // console.log(data);
-    //         // newData[i].miles = formatMiles(data[i].miles);
-    // }
+    //             // console.log(data);
+    //             // newData[i].miles = formatMiles(data[i].miles);
+    //     }
     // };
-    // let newData = data; // Use local data object until api access is fixed
-//     cleanData(data);
-// }
+      // let newData = data; // Use local data object until api access is fixed
+    //   cleanData(data);
+  }
+  
+
+  function updateFilterSummary() {
+    // Clear all items before drawing new
+    var filterSummary = document.getElementById('filterSummary');
+    filterSummary.innerHTML = '';
+  
+    // Loop thru applied filters and render summary "buttons"
+    var selectItemSex = document.getElementById('selectSex');
+    var selectItemAgeGroup = document.getElementById('selectAgeGroup');
+    var selectItemLmsc = document.getElementById('selectLmsc');
+    var selectItemZone = document.getElementById('selectZone');
+    var filterItems = [selectItemSex, selectItemAgeGroup, selectItemLmsc, selectItemZone];
+    for (i = 0; i < filterItems.length; i++) {
+      if (filterItems[i].value !== 'All') {
+        var el = document.createElement('p');
+        var elParent = document.getElementById('filterSummary');
+        el.className = "filters__summary--item filters__summary--item-" + filterItems[i].parentElement.classList.value;
+        el.id = filterItems[i].parentElement.classList.value;
+        el.textContent = filterItems[i].value;
+        elParent.append(el);
+      }    
+    }
 
 
 
+  function handleFilters() {
 
+    // var selectValueMilestone = document.getElementById('selectMilestone').value;
+  
+    
 
+    // gtdMilestones.sort(age, {
+    //     order: 'desc'
+    // })
 
-// milestoneTemplate
-// gtdMilestones
+    // gtdMilestones.filter(function(item) {
+    //     if (item.values().milestone > selectValueMilestone) {
+    //         console.log("YES!!!!!");
+    //        return true;
+    //     } else {
+    //        return false;
+    //     }
+    // });
 
-// function createHtml(milestoneTemplate) {
-//     var rawTemplate = document.getElementById("milestoneTemplate").innerHTML;
-//     var compiledTemplate = Handlebars.compile(rawTemplate);
-//     var generatedHTML = compiledTemplate(milestoneTemplate)
-//     var gtdMilestones = document.getElementById("gtdMilestones");
-//     gtdMilestones.innerHTML = generatedHTML;
-// }
+    // if (selectValueMilestone !== "All") {
+    //     // console.log(selectValueMilestone);
+    //     // gtdMilestones.filter(function(item) {
+    //     //     return item.values().milestone === selectValueMilestone;
+    //     // });
+    //     gtdMilestones.filter('milestone', selectValueMilestone );
+    // } else {
+    //     gtdMilestones.filter();
+    // }
 
-
-
-
-
-// function updateFilterSummary() {
-//   // Clear all items before drawing new
-//   var filterSummary = document.getElementById('filterSummary');
-//   filterSummary.innerHTML = '';
-
-//   // Loop thru applied filters and render summary "buttons"
-//   var selectItemSex = document.getElementById('selectSex');
-//   var selectItemAgeGroup = document.getElementById('selectAgeGroup');
-//   var selectItemLmsc = document.getElementById('selectLmsc');
-//   var selectItemZone = document.getElementById('selectZone');
-//   var filterItems = [selectItemSex, selectItemAgeGroup, selectItemLmsc, selectItemZone];
-//   for (i = 0; i < filterItems.length; i++) {
-//     if (filterItems[i].value !== 'All') {
-//       var el = document.createElement('p');
-//       var elParent = document.getElementById('filterSummary');
-//       el.className = "filters__summary--item filters__summary--item-" + filterItems[i].parentElement.classList.value;
-//       el.id = filterItems[i].parentElement.classList.value;
-//       el.textContent = filterItems[i].value;
-//       elParent.append(el);
-//     }    
-//   }
-
-  // Remove filter summary "buttons"
-//   $("#sex").click(function(){
-//     document.getElementById("selectSex").selectedIndex = 0;
-//     handleFilters();
-//   });
-// }
-
-// function handleFilters(e) {
-//   var selectValueLmsc = document.getElementById('selectLmsc').value;
-// // One Value Defs
-//   if (selectValueSex !== "All" && selectValueAgeGroup === "All" && selectValueLmsc === "All" && selectValueZone === "All" ) {
-//     gtdMilestones.filter(function(item) {
-//       return item.values().sex === selectValueSex;
-//     });
-//   } else {
-//     gtdMilestones.filter();
-//   }
-//   updateFilterSummary();
-// }
-
-
-// $(".select").change(function(){
-//   handleFilters();
-// });
+  }
+  
+  
+  $(".select").change(function(){
+    handleFilters();
+  });
+  
 
 }
-
-// $("#clearFilters").click(function(){
-//   searchName.value = '';
-//   $("select").each(function() { this.selectedIndex = 0 });
-//   gtdMilestones.filter();
-//   gtdMilestones.search();
-//   gtdMilestones.sort('miles', {order:'desc'});
-//   handleSearch();
-//   // updateSearchSummary();
-//   handleFilters();
-//   // updateFilterSummary();
-// });
