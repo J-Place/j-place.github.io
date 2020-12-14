2a6cc2d79570c244488a4533eb25eb1d5c69a460
J-Place <jplace@usmastersswimming.org>
2020-12-14 16:51:38 -0500

    no message

--- js/gtdMilestones.js
+++ js/gtdMilestones.js
@@ -4,16 +4,13 @@ function hideLoadingSpinner() {
   const loadingSpinner = document.getElementById('loadingWrapper');
   loadingSpinner.classList.add('hide');
 }
+
 var getData = $.ajax({
   url: "https://j-place.github.io/milestones.json",
   type: "GET",
   success: function (response) {
     var data = JSON.parse(getData.responseText);
-    // console.log(data.data[0]);
-    // console.log(data.data[0].milestone);
     var dataLocal = data.data;
-    // handleFilters(dataLocal);
-    // cleanData(dataLocal);
     gtdMilestones = new List('gtdMilestones', options, dataLocal);
     gtdMilestones.sort(
       'milestone', {
@@ -36,31 +33,33 @@ var options = {
   valueNames: ['milestone', 'first', 'last', 'age', 'sex', 'clubAbbr', 'dateAchieved'],
 }
 
-function handleFilters(e) {
+function handleFilters() {
+  var selectValueMilestone = document.getElementById('selectMilestone').value;
   if (!gtdMilestones) {
     console.error('gtdMilestones not defined');
     return;
   }
-  var selectValueMilestone = document.getElementById('selectMilestone').value;
-  console.log(selectValueMilestone);
-  gtdMilestones.sort(milestone, {
-      order: 'desc'
-  })
+  // if (selectValueMilestone !== "All") {
+  //     gtdMilestones.filter('milestone', selectValueMilestone );
+  // } else {
+  //     gtdMilestones.filter();
+  // }
+  // gtdMilestones.sort(milestone, {
+  //   order: 'desc'
+  // })
+
   gtdMilestones.filter(function(item) {
       if (item.values().milestone > selectValueMilestone) {
-         return true;
+        console.log("True");
+        return true;
       } else {
-         return false;
+        console.log("False");
+        return false;
       }
   });
-  if (selectValueMilestone !== "All") {
-      gtdMilestones.filter('milestone', selectValueMilestone );
-  } else {
-      gtdMilestones.filter();
-  }
 }
+
 $(".select").change(function () {
-  // console.log("Handling filters ...");
   handleFilters();
 });
 
