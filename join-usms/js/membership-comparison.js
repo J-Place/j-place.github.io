0faf8dbf6bddd036c3218ad056648921c4abfb5d
J.Place <jplace@usmastersswimming.org>
2022-03-28 15:06:22 -0400

    add js for comparison detail

---
+++ join-usms/js/membership-comparison.js
@@ -0,0 +1,30 @@
+
+// var compareDetailsTable = document.getElementById(compareDetails);
+// var compareDetailsBtn = document.getElementById(compareDetailsBtn);
+
+// alert("click");
+// console.log(compareDetailsBtn);
+// document.getElementById(compareDetailsBtn).click(function() {
+    // alert("click");
+    // console.log('Test');
+    // document.getElementById(compareDetails).style.display = 'block';
+    // document.getElementById( 'elemtId' ).style.display = 'none';
+// });
+
+
+
+function handleTableDetails() {
+    // alert("click");
+    document.getElementById("compareDetails").style.height = '100%';
+    // document.getElementById("compareDetails").style.display = 'block';
+    
+}
+
+
+
+
+
+
+
+
+// onclick="handleTableDetails()"
