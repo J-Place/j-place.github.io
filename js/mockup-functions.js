aba987576ff40b3a44b735508a4d151afd4dd78e
J.Place <jplace@usmastersswimming.org>
2022-04-13 11:34:31 -0400

    cruft

--- js/mockup-functions.js
+++ js/mockup-functions.js
@@ -1,6 +1,5 @@
 function handleShowFilters() {
-
-    var toggle = document.querySelector('button.toggle-filters');
+    // var toggle = document.querySelector('button.toggle-filters');
     var toggleContainer = document.querySelector('div.list-control-filter');
 
     if (toggleContainer.classList.contains('show-filters')) {
@@ -10,18 +9,6 @@ function handleShowFilters() {
         console.log("Closed");
         toggleContainer.classList.add('show-filters');
     }
-
-    // console.log(toggle.id);
-    // console.log(toggleContainer);
-    // console.log(toggleClass);
-    // if (toggle.classList.contains('show-filters')) {
-    //     console.log("Hide Filters");
-    //     toggleContainer.classList.remove('show-filters');
-    // } else {
-    //     console.log("Show Filters");
-    //     toggleContainer.classList.add('show-filters');
-    // }
-
 }
 
 
