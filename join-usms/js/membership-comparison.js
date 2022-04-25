b981d00871eb1b68aab0b90ae3756f74652ae613
J.Place <jplace@usmastersswimming.org>
2022-04-25 13:45:01 -0400

    updates

--- join-usms/js/membership-comparison.js
+++ join-usms/js/membership-comparison.js
@@ -1,4 +1,72 @@
 
+
+
+
+
+
+
+
+var heightHeader = 0;
+$('.personalize__content-column-header').each(function(){  
+        if($(this).height() > heightHeader){  
+        heightHeader = $(this).height();  
+    }
+});    
+$('.personalize__content-column-header').height(heightDescription);
+
+var heightDescription = 0;
+$('.personalize__content-column-description').each(function(){  
+        if($(this).height() > heightDescription){  
+        heightDescription = $(this).height();  
+    }
+});    
+$('.personalize__content-column-description').height(heightDescription);
+
+
+
+
+
+
+(function () {
+    equalHeight(false);
+  })();
+   
+  window.onresize = function(){
+    equalHeight(true);
+  }
+   
+  function equalHeight(resize) {
+    var elementsHeader = document.getElementsByClassName("personalize__content-column-header");
+    var elementsDescription = document.getElementsByClassName("personalize__content-column-description");
+    var allHeightsHeader = [];
+    var allHeightsDescription = [];
+    i = 0;
+    if(resize === true){
+      for(i = 0; i < elementsHeader.length; i++){
+        elementsHeader[i].style.height = 'auto';
+        elementsDescription[i].style.height = 'auto';
+      }
+    }
+    for(i = 0; i < elementsHeader.length; i++){
+      var elementHeightHeader = elementsHeader[i].clientHeight;
+      var elementHeightDescription = elementsHeader[i].clientHeight;
+      allHeightsHeader.push(elementHeightHeader);
+      allHeightsDescription.push(elementHeightDescription);
+    }
+    for(i = 0; i < elementsHeader.length; i++){
+      elementsHeader[i].style.height = Math.max.apply( Math, allHeightsHeader) + 'px';
+      elementsDescription[i].style.height = Math.max.apply( Math, allHeightsDescription) + 'px';
+      // if(resize === false){
+      //   elementsHeader[i].className = elementsHeader[i].className + " showXXX";
+      // }
+    }
+  }
+
+
+
+  
+
+
 // var compareDetailsTable = document.getElementById(compareDetails);
 // var compareDetailsBtn = document.getElementById(compareDetailsBtn);
 // document.getElementById(compareDetailsBtn).click(function() {
