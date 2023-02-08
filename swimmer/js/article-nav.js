8a1e09be2d20ea84001f245a0571957ce5e2097b
J.Place <jplace@usmastersswimming.org>
2023-02-08 17:31:28 -0500

    set up sticky nav script

--- swimmer/js/article-nav.js
+++ swimmer/js/article-nav.js
@@ -1,27 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
     const articleNav = document.querySelector('.article-nav');
-    const footerPos = document.querySelector('footer');
     const articleTitle = document.querySelector('.feature-title');
     const relatedArticles = document.querySelector('.article-stepper');
+    const relatedArticlesHeight = relatedArticles.offsetHeight;
     const articleStart = articleTitle.offsetParent.offsetTop;
-    const articleEnd = relatedArticles.offsetParent.offsetTop;
-    // if (window.scrollY >= 680) {    
-    //     articleNav.classList.add('sticky-start');
-    // }
-    // console.log(scrollY);
-    // console.log(articleStart, articleEnd);
+    const relatedArticlesTop = relatedArticles.offsetParent.offsetTop;
+    const articleEnd = relatedArticlesTop + relatedArticlesHeight;
+    const articleNavHeight = articleNav.offsetHeight;
+
+    // Set position on page load
+    if (window.scrollY >= articleStart  && window.scrollY <= articleEnd) {
+        // articleNav.style.top = articleStart + 'px';
+        articleNav.style.top = 0 + 'px';
+        articleNav.classList.add('sticky-start');
+        articleNav.classList.remove('sticky-end');
+    } else {
+        articleNav.style.top = articleStart + 'px';
+    }
+
+    // Set position on scroll
     function stickynavbar() {
-        // console.log(articleStart);
-        // console.log(scrollY);
-        // console.log(articleEnd.offsetTop);
-        if (window.scrollY >= articleStart && window.scrollY <= articleEnd) {    
-            articleNav.classList.add('sticky-start');
-        }  else if (window.scrollY >= articleEnd) {
+        if (window.scrollY < articleStart) {
+            articleNav.style.top = articleStart + 'px';
             articleNav.classList.remove('sticky-start');
             articleNav.classList.add('sticky-end');
-        } 
-        else {
-            articleNav.classList.remove('sticky-start', 'sticky-end');
+        }
+        if (!articleNav.classList.contains('sticky-start') && !articleNav.classList.contains('sticky-end') && window.scrollY >= articleStart && window.scrollY <= articleEnd) {    
+            articleNav.classList.add('sticky-start');
+            articleNav.style.top = 0 + 'px';
+        }
+        if (articleNav.classList.contains('sticky-start') && window.scrollY >= articleStart && window.scrollY >= articleEnd) {
+            articleNav.classList.remove('sticky-start');
+            articleNav.classList.add('sticky-end');
+            articleNav.style.top = articleEnd + articleNavHeight + 'px';
+        }
+        // Scroll back up after scrolling down
+        if (!articleNav.classList.contains('sticky-start') && articleNav.classList.contains('sticky-end') && window.scrollY >= articleStart && window.scrollY <= articleEnd + articleNavHeight) {
+            articleNav.classList.add('sticky-start');
+            articleNav.classList.remove('sticky-end');
+            articleNav.style.top = 0 + 'px';
         }
     }
     window.addEventListener('scroll', stickynavbar);
