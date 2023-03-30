2d0b1ee69d554ddfbc9414cc6ff96390e0154f18
J.Place <jplace@usmastersswimming.org>
2023-03-29 21:23:43 -0400

    sticky ad in progress

--- swimmer/js/sticky-nav.js
+++ swimmer/js/sticky-nav.js
@@ -1,38 +1,52 @@
 var mobile = window.matchMedia("screen and (max-width:480px)").matches;
 if (mobile) {
-    document.querySelector('.article-nav').classList.add('mobile');
+    document.querySelector('.sticky-nav').classList.add('mobile');
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const articleNav = document.querySelector('.article-nav');
-    const articleNavHeight = articleNav.offsetHeight;
+    const stickyNav = document.querySelector('.sticky-nav');
+    const stickyNavLeft = document.querySelector('.sticky-nav.sticky-nav-left');
+    // const allAds = document.querySelectorAll('.sticky-ad');
+    const stickyNavRight = document.querySelector('.sticky-nav.sticky-nav-right');
+    const stickyNavHeight = stickyNav.offsetHeight;
     // const relatedArticlesTop = relatedArticles.offsetParent.offsetTop;
     const articleStart = document.querySelector('.article-body').offsetTop;
     const articleEnd = document.querySelector('.article-end').offsetTop;
 
+    const usmsContainer = document.querySelector('.usms-container').clientWidth;
+    const windowContainer = document.querySelector('body').clientWidth;
+
+    const stickyAdMargin = (windowContainer - usmsContainer) / 2;
+
+    // console.log((windowContainer - usmsContainer) / 2);
+    console.log(stickyAdMargin);
+
     if (window.scrollY >= articleStart && window.scrollY <= articleEnd) {
         // Set nav position on initial page load
-        // articleNav.style.top = 0 + 'px';
-        articleNav.classList.add('sticky-start');
-        articleNav.classList.remove('sticky-end');
+        // stickyNav.style.top = 0 + 'px';
+        stickyNavRight.classList.add('sticky-start');
+        // stickyNav.classList.add('sticky-start');
+        
+        stickyNavLeft.classList.remove('sticky-end');
+        stickyNavRight.classList.remove('sticky-end');
     } else { 
         // Set nav position on page reload or loading page from hash links
         if (mobile) {
-            articleNav.style.top = 30 + 'px';
+            stickyNav.style.top = 30 + 'px';
         } else if (!mobile) {
-            // articleNav.style.top = articleStart + 'px';
+            // stickyNav.style.top = articleStart + 'px';
         }
     }
 
     let mobileNavList = document.querySelector('#navList');
-    let mobileNavButton = document.querySelector('.article-nav-title');
+    let mobileNavButton = document.querySelector('.sticky-nav-title');
     document.body.addEventListener('click', function(event){
         if (mobile && window.scrollY < articleStart - 70) {
             document.querySelector('.article-body').scrollIntoView();
             console.log("DOES THIS EVER FIRE?");
         }
         if (mobile && window.scrollY < articleStart && mobileNavButton.contains(event.target)) {
-            articleNav.style.top = 0 + 'px';            
+            stickyNav.style.top = 0 + 'px';            
             console.log("Freeze");
         }
         if (mobile && mobileNavList.contains(event.target)){
@@ -48,69 +62,77 @@ document.addEventListener("DOMContentLoaded", function () {
     function setNavPosition() {
         if (mobile) {
             if (window.scrollY < articleStart) {                
-                articleNav.style.top = 0 + 'px';
-                articleNav.classList.remove('sticky-start');
-                articleNav.classList.remove('sticky-end');
+                stickyNav.style.top = 0 + 'px';
+                stickyNav.classList.remove('sticky-start');
+                stickyNav.classList.remove('sticky-end');
             }
             if (window.scrollY >= articleStart) {
-                articleNav.style.top = 0 + 'px';
-                articleNav.classList.add('sticky-start');
-                articleNav.classList.remove('sticky-end');
+                stickyNav.style.top = 0 + 'px';
+                stickyNav.classList.add('sticky-start');
+                stickyNav.classList.remove('sticky-end');
             }
-            if (!articleNav.classList.contains('sticky-start') 
-                && !articleNav.classList.contains('sticky-end') 
+            if (!stickyNav.classList.contains('sticky-start') 
+                && !stickyNav.classList.contains('sticky-end') 
                 && window.scrollY >= articleStart 
                 && window.scrollY <= articleEnd) {    
-                articleNav.classList.add('sticky-start');
-                articleNav.style.top = 0 + 'px';
+                stickyNav.classList.add('sticky-start');
+                stickyNav.style.top = 0 + 'px';
             }
-            if (!articleNav.classList.contains('sticky-start') 
-                && articleNav.classList.contains('sticky-end') 
+            if (!stickyNav.classList.contains('sticky-start') 
+                && stickyNav.classList.contains('sticky-end') 
                 && window.scrollY >= articleStart 
-                && window.scrollY <= articleEnd + articleNavHeight) {
-                articleNav.classList.add('sticky-start');
-                articleNav.classList.remove('sticky-end');
-                articleNav.style.top = 0 + 'px';
+                && window.scrollY <= articleEnd + stickyNavHeight) {
+                stickyNav.classList.add('sticky-start');
+                stickyNav.classList.remove('sticky-end');
+                stickyNav.style.top = 0 + 'px';
             }
-            if (articleNav.classList.contains('sticky-start') 
+            if (stickyNav.classList.contains('sticky-start') 
                 && window.scrollY >= articleStart 
-                && window.scrollY >= articleEnd - articleNavHeight - articleNavHeight) {
-                articleNav.classList.remove('sticky-start');
-                articleNav.classList.add('sticky-end');
-                articleNav.style.top = 0 + 'px';
+                && window.scrollY >= articleEnd - stickyNavHeight - stickyNavHeight) {
+                stickyNav.classList.remove('sticky-start');
+                stickyNav.classList.add('sticky-end');
+                stickyNav.style.top = 0 + 'px';
             }
         } else if (!mobile) {
             if (window.scrollY < articleStart 
-                && !articleNav.classList.contains('sticky-start')) {
-                articleNav.style.top = 0 + 'px';
+                && !stickyNav.classList.contains('sticky-start')) {
+                // stickyNav.style.top = 0 + 'px';
             }
             if (window.scrollY < articleStart 
-                && articleNav.classList.contains('sticky-start')) {
-                articleNav.classList.remove('sticky-start');
-                articleNav.style.top = 0 + 'px';
+                && stickyNav.classList.contains('sticky-start')) {
+                stickyNav.classList.remove('sticky-start');
+                stickyNavRight.classList.remove('sticky-start');
+                // stickyNav.style.top = 0 + 'px';
+                stickyNavLeft.style.left = 0 + 'px';
+                stickyNavRight.style.right = 0 + 'px';
             }
             if (window.scrollY >= articleStart 
                 && window.scrollY <= articleEnd
-                && !articleNav.classList.contains('sticky-start') 
-                && !articleNav.classList.contains('sticky-end')) {    
-                articleNav.classList.add('sticky-start');
-                articleNav.style.top = 0 + 'px';
+                && !stickyNav.classList.contains('sticky-start') 
+                && !stickyNav.classList.contains('sticky-end')) {    
+                stickyNav.classList.add('sticky-start');
+                stickyNavRight.classList.add('sticky-start');
+                // stickyNav.style.top = 0 + 'px';
+                stickyNavLeft.style.left = stickyAdMargin + 22 + 'px';
+                stickyNavRight.style.right = stickyAdMargin + 22 + 'px';
                 console.log("SAT");
             }
             if (window.scrollY >= articleStart 
-                && window.scrollY <= articleEnd - articleNavHeight 
-                && !articleNav.classList.contains('sticky-start') 
-                && articleNav.classList.contains('sticky-end')) {
-                articleNav.classList.add('sticky-start');
-                articleNav.classList.remove('sticky-end');
-                articleNav.style.top = 0 + 'px';
+                && window.scrollY <= articleEnd - stickyNavHeight 
+                && !stickyNav.classList.contains('sticky-start') 
+                && stickyNav.classList.contains('sticky-end')) {
+                stickyNav.classList.add('sticky-start');
+                stickyNavRight.classList.add('sticky-start');
+                stickyNav.classList.remove('sticky-end');
+                // stickyNav.style.top = 0 + 'px';
                 console.log("ACT");
             }
-            if (window.scrollY >= articleEnd - articleNavHeight
-                && articleNav.classList.contains('sticky-start')) {
-                articleNav.classList.remove('sticky-start');
-                articleNav.classList.add('sticky-end');
-                articleNav.style.top = 0 + 'px';
+            if (window.scrollY >= articleEnd - stickyNavHeight
+                && stickyNav.classList.contains('sticky-start')) {
+                stickyNav.classList.remove('sticky-start');
+                stickyNavRight.classList.remove('sticky-start');
+                stickyNav.classList.add('sticky-end');
+                // stickyNav.style.top = 0 + 'px';
                 console.log("TST");
             }    
         }
