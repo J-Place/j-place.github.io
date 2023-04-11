aede311d6193155ae726e629e5726ec3b24eee5b
J.Place <jplace@usmastersswimming.org>
2023-04-11 10:01:34 -0400

    sticky ad 9-3 layout

--- js/sticky-ad.js
+++ js/sticky-ad.js
@@ -1,82 +1,55 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    let stickyHeight = 0;    
-
-    const mobile = window.matchMedia("screen and (max-width:991px)").matches; // window.innerWidth < 992;
+    let stickyHeight = 0;
     const viewSm = window.matchMedia("screen and (min-width:576px)").matches;
     const viewMd = window.matchMedia("screen and (min-width:768px)").matches;
-    const viewLg = window.matchMedia("screen and (min-width:992px)").matches;
-    const viewXl = window.matchMedia("screen and (min-width:1200px)").matches;
-    const viewXxl = window.matchMedia("screen and (min-width:1400px)").matches;
 
     // Sets height for known ad sizes
     if (viewSm) {
         stickyHeight = 255;
     } else if (viewMd) {
         stickyHeight = 315;
-    } else if (viewLg) {
-        stickyHeight = 315;
-    } else if (viewXl) {
-        stickyHeight = 315;
-    } else if (viewXxl) {
-        stickyHeight = 315;
     }
-
-    const stickyAd = document.querySelector('.ad__space'); // JP: create a new class to toggle via rendering parameter, this is temp for testing in QA
+    const stickyAd = document.querySelector('.ad__space'); // JP: create a new class to toggle via rendering parameter
     const stickyAds = document.querySelectorAll('.ad__space');
-    const mainContent = document.querySelector('header + .section');
-    // const mainContentTop = mainContent.getBoundingClientRect().top;
-    // const mainContentHeight = mainContent.getBoundingClientRect().height;
-    const mainContentHeight = mainContent.scrollHeight;
+    const articleBylineHeight = document.querySelector('.article__byline').clientHeight;
+    const articleBodyHeight = document.querySelector('.article__body').clientHeight;
+    const mainContentHeight = articleBylineHeight + articleBodyHeight;
     const stickyTop = stickyAd.parentElement.offsetTop;
-    const stickyBottom = stickyHeight*4 + mainContentHeight;
-    const footer = document.querySelector('footer');
+    // const stickyBottom = mainContentHeight - stickyHeight;
+    const stickyBottom = mainContentHeight;
 
     function setAdTop() {
         for (let i = 0; i < stickyAds.length; i++) {
-            // if (window.scrollY < stickyTop) {
-            //     stickyAds[i].style.top = stickyTop + 'px';
-            // }
             stickyAd.style.top = stickyTop + 'px';
         }
     }
     setAdTop();
 
     function handleAdPosition() {
-        var scrollTop = window.pageYOffset; // window.scrollTop;
-        if (!mobile && scrollTop === 0) {
-            // console.log("At the top ...");
-            // for (let i = 0; i < stickyAds.length; i++) {
-            //     stickyAds[i].style.display = 'block';
-            //     stickyAds[i].style.top = stickyTop + 'px';
-            // }
+        const contentWidth = document.querySelector('.usms-container').offsetWidth;
+        const windowWidth = window.innerWidth;
+        const contentMargins = windowWidth - contentWidth;
+        const contentMargin = contentMargins/2;
+        const contentMarginWhole = Math.round(contentMargin);
+        stickyAd.style.right = contentMarginWhole + 7 + 'px';
+
+        var scrollTop = window.pageYOffset;
+
+        if (viewSm && scrollTop === 0) {
             stickyAd.style.display = 'block';
             stickyAd.style.top = stickyTop + 'px';
         }
-        if (!mobile && scrollTop <= stickyTop) {
-            // console.log("Scroll is above main content ...");
-            // for (let i = 0; i < stickyAds.length; i++) {
-            //     stickyAds[i].style.display = 'block';
-            //     stickyAds[i].style.top = stickyTop - window.pageYOffset + 'px';
-            // }
+        if (viewSm && scrollTop <= stickyTop) {
             stickyAd.style.display = 'block';
             stickyAd.style.top = stickyTop - window.pageYOffset + 'px';
     }
-        if (!mobile && scrollTop > stickyTop) {
-            // console.log("Scroll is in main content ...");
-            // for (let i = 0; i < stickyAds.length; i++) {
-            //     stickyAds[i].style.display = 'block';
-            //     stickyAds[i].style.top = 10 + 'px';
-            // }
+        if (viewSm && scrollTop > stickyTop) {
             stickyAd.style.display = 'block';
             stickyAd.style.top = 10 + 'px';
     }
-        if (!mobile && scrollTop >= stickyBottom) {
-            // console.log("Scroll is below main content ...");
-            // for (let i = 0; i < stickyAds.length; i++) {
-            //     stickyAds[i].style.display = 'none';
-            // }
+        if (viewSm && scrollTop >= stickyBottom) {
             stickyAd.style.display = 'none';
         }
     }
@@ -92,4 +65,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener('scroll', scroll);
     window.addEventListener('resize', resize);
+
 });
