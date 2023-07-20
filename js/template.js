ea25c3e62862f950d856768ce3d1705d32e781f6
J.Place <jplace@usmastersswimming.org>
2023-07-20 12:25:15 -0400

    set header and nav to mobile at 1200px

--- js/template.js
+++ js/template.js
@@ -1,10 +1,15 @@
-const Handlebars = require("handlebars");
-const template = Handlebars.compile("Name: {{name}}");
-console.log(template({ name: "Nils" }));
+  // compile the template
+  var template1 = Handlebars.compile("{{body}}");
+  console.log(template1({ body: "1 rocks!" }));
+
+
+var template2 = Handlebars.compile("{{doesWhat}}");
+// execute the compiled template and print the output to the console
+console.log(template2({ doesWhat: "2 rocks!" }));
 
 
 
 
-// function() {
-    console.log('Hello!');
-// }
+function getHeader() {
+    var header = {}
+}
