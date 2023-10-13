716a17a680c6ef6b6cc92418506d7e7fc6e269df
J.Place <jplace@usmastersswimming.org>
2023-10-13 15:02:30 -0400

    no message

--- js/article-signup-mockup.js
+++ js/article-signup-mockup.js
@@ -1,18 +1,17 @@
 
-const articleSignupBtn = document.getElementById("sign-up__button");
-articleSignupBtn.addEventListener("click", toggleSignup);
+// const articleSignupBtn = document.getElementById("sign-up__button");
+// articleSignupBtn.addEventListener("click", toggleSignup);
+const signupTarget = document.getElementsByClassName("sign-up--inline");
+signupTarget[0].addEventListener("click", toggleSignup);
 
 function toggleSignup() {    
     const signupContainer = document.getElementsByClassName("sign-up__container");
     const signupSuccess = document.getElementsByClassName("sign-up__confirmation");
-    // console.log(signupContainer[1]);
     if (signupContainer[0].style.display === "block") {
-      signupContainer[0].style.display = "none";
-      signupSuccess[0].style.display = "block";
-        // console.log("Yes");
+        signupContainer[0].style.display = "none";
+        signupSuccess[0].style.display = "block";
     } else {
-      signupContainer[0].style.display = "block";
-      signupSuccess[0].style.display = "none";
-        // console.log("No");
+        signupContainer[0].style.display = "block";
+        signupSuccess[0].style.display = "none";
     }
 }
