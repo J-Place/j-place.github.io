e7c81c98c2a6cef4126230da76bb636671fc785c
J.Place <jplace@usmastersswimming.org>
2023-10-17 10:25:29 -0400

    add article signup modal

--- js/article-signup-mockup.js
+++ js/article-signup-mockup.js
@@ -1,17 +1,6 @@
 
-// const articleSignupBtn = document.getElementById("sign-up__button");
-// articleSignupBtn.addEventListener("click", toggleSignup);
-const signupTarget = document.getElementsByClassName("sign-up--inline");
-signupTarget[0].addEventListener("click", toggleSignup);
+// const signupContainer = document.getElementsByClassName("sign-up--inline");
 
-function toggleSignup() {    
-    const signupContainer = document.getElementsByClassName("sign-up__container");
-    const signupSuccess = document.getElementsByClassName("sign-up__confirmation");
-    if (signupContainer[0].style.display === "block") {
-        signupContainer[0].style.display = "none";
-        signupSuccess[0].style.display = "block";
-    } else {
-        signupContainer[0].style.display = "block";
-        signupSuccess[0].style.display = "none";
-    }
-}
+$("#articleSignupContinue").on("click", function(){
+    $('.sign-up--inline').css('display','none');
+});
