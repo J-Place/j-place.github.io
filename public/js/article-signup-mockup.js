3361898756d8f551d8318d4ab4e87b8419c74fcb
J.Place <jplace@usmastersswimming.org>
2023-10-17 12:23:36 -0400

    no message

--- public/js/article-signup-mockup.js
+++ public/js/article-signup-mockup.js
@@ -1,12 +1,7 @@
-
-// const signupContainer = document.getElementsByClassName("sign-up--inline");
-
 $("#articleSignupContinue").on("click", function(){
     $('.sign-up--inline').css('display','none');
 });
 
-// var myModal = document.getElementById('articleSignupDuplicateModal');
-
-// $("#emailAddress").on("focus", function(){
-//     myModal.show();
-// })
+$("#articleSignupDuplicateContinue").on("click", function(){
+    $('.sign-up--inline').css('display','none');
+});
