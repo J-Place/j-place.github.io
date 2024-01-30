448bdfd4f89f3ba68f692afcfb74f7cc6184b714
J.Place <jplace@usmastersswimming.org>
2024-01-30 11:33:11 -0500

    javascript for plan selection UI

---
+++ lane-mate/js/lanemate.js
@@ -0,0 +1,21 @@
+
+$("#confirmLaneMateMonthlyPlan").on( "click", function() {
+    $(".current-plan__summary").removeClass("current-plan--selected");
+    $(".current-plan_lane-mate-monthly").addClass("current-plan--selected");
+    $(".plan-option").removeClass("plan-option-selected");
+    $(".plan-option_lane-mate-monthly").addClass("plan-option-selected");
+});
+
+$("#confirmLaneMateYearlyPlan").on( "click", function() {
+    $(".current-plan__summary").removeClass("current-plan--selected");
+    $(".current-plan_lane-mate-yearly").addClass("current-plan--selected");
+    $(".plan-option").removeClass("plan-option-selected");
+    $(".plan-option_lane-mate-yearly").addClass("plan-option-selected");
+});
+
+$("#confirmUsmsYearlyPlan").on( "click", function() {
+    $(".current-plan__summary").removeClass("current-plan--selected");
+    $(".current-plan_usms-yearly").addClass("current-plan--selected");
+    $(".plan-option").removeClass("plan-option-selected");
+    $(".plan-option_usms-yearly").addClass("plan-option-selected");
+});
