5447a312622f89acf37858114f872b20266a0c5f
J.Place <jplace@usmastersswimming.org>
2024-01-31 13:14:34 -0500

    meganav in place

--- public/college-club/js/college-club.js
+++ public/college-club/js/college-club.js
@@ -1,13 +1,30 @@
 
-$("#menu-item-0").on( "click", function() {
-    $(".mega-menu-overlay").hide();
-    $("#megaMenu0").toggle();
-    // $(".mega-main-menu__items > li").removeClass('active');
-    // $("#menu-item-0").addClass('active');
+// $("#menu-item-0").on( "click", function() {
+//     // $(".mega-menu-overlay").hide();
+//     $("#megaMenu0").toggle();
+//     // $(".mega-main-menu__items > li").removeClass('active');
+//     // $("#menu-item-0").addClass('active');
+// });
+// $("#menu-item-1").on( "click", function() {
+//     // $(".mega-menu-overlay").hide();
+//     $("#megaMenu1").toggle();
+//     // $(".mega-main-menu__items > li").removeClass('active');
+//     // $("#menu-item-1").addClass('active');
+// });
+
+
+
+$('.mega-main-menu__items li').click(function() {    
+    var target = $(this).data('target');
+    // $('#' + target).slideToggle();
+    $('#' + target).toggle();
+    $('.mega-menu-overlay').not('#' + target).hide();
 });
-$("#menu-item-1").on( "click", function() {
-    $(".mega-menu-overlay").hide();
-    $("#megaMenu1").toggle();
-    // $(".mega-main-menu__items > li").removeClass('active');
-    // $("#menu-item-1").addClass('active');
+
+$(document).on('click', function(event) {
+    var target = $(event.target);
+    // Check if the clicked element is not a menu item or dropdown
+    if (!target.closest('.mega-main-menu__items li, .mega-menu-overlay').length) {
+        $('.mega-menu-overlay').hide();
+    }
 });
