411e8a3c8314367f98eb25bbf34e37ca2cf3eac5
J.Place <jplace@usmastersswimming.org>
2024-01-31 12:27:41 -0500

    add megamenu component - html and javascript

---
+++ public/college-club/js/college-club.js
@@ -0,0 +1,13 @@
+
+$("#menu-item-0").on( "click", function() {
+    $(".mega-menu-overlay").hide();
+    $("#megaMenu0").toggle();
+    // $(".mega-main-menu__items > li").removeClass('active');
+    // $("#menu-item-0").addClass('active');
+});
+$("#menu-item-1").on( "click", function() {
+    $(".mega-menu-overlay").hide();
+    $("#megaMenu1").toggle();
+    // $(".mega-main-menu__items > li").removeClass('active');
+    // $("#menu-item-1").addClass('active');
+});
