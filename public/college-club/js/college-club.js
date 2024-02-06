cc0c921bd28b37dfa8497eed31f9808f288aaa9f
J.Place <jplace@usmastersswimming.org>
2024-02-06 14:50:16 -0500

    no message

--- public/college-club/js/college-club.js
+++ public/college-club/js/college-club.js
@@ -12,21 +12,22 @@
 //     // $("#menu-item-1").addClass('active');
 // });
 
-
-
 $('.mega-main-menu__items li').click(function() {
-    $(this).toggleClass("active");
     var target = $(this).data('target');
-    // $('#' + target).slideToggle();
-    $('#' + target).toggle();
-    $('.mega-menu-overlay').not('#' + target).hide();
+    $('.mega-main-menu__items li').removeClass("active");
+    $(this).addClass("active");
+    $('body').addClass('no-scroll');
+    $('.my-element').css('z-index', 100);
+    $('#' + target).addClass("is-flex");
+    // $('.mega-menu-overlay').not('#' + target).hide();
+    $('.mega-menu-overlay').not('#' + target).removeClass('is-flex');
 });
 
-$(document).on('click', function(event) {
-    $('.mega-main-menu__items li').removeClass("active");
+$(document).on('click', function(event) {    
     var target = $(event.target);
     // Check if the clicked element is not a menu item or dropdown
     if (!target.closest('.mega-main-menu__items li, .mega-menu-overlay').length) {
-        $('.mega-menu-overlay').hide();
+        $('.mega-menu-overlay').removeClass("is-flex");
+        $('body').removeClass('no-scroll');
     }
 });
