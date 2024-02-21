07fd8be1d0e89ccf8107bbb2148e77a20a57bf1a
J.Place <jplace@usmastersswimming.org>
2024-02-21 14:02:56 -0500

    add cancel auto renew javascript, add member reg sections

--- lane-mate/js/lanemate.js
+++ lane-mate/js/lanemate.js
@@ -2,6 +2,8 @@
 // $("").addClass("");
 
 $("#confirmLaneMateMonthlyPlan").on( "click", function() {
+    $(".current-plan__renewal-date").removeClass("current-plan--selected");
+    $(".current-plan__renewal-date__lane-mate-monthly").addClass("current-plan--selected");
     $(".current-plan__summary").removeClass("current-plan--selected");
     $(".current-plan_lane-mate-monthly").addClass("current-plan--selected");
     $(".plan-option").removeClass("plan-option-selected");
@@ -9,6 +11,8 @@ $("#confirmLaneMateMonthlyPlan").on( "click", function() {
 });
 
 $("#confirmLaneMateYearlyPlan").on( "click", function() {
+    $(".current-plan__renewal-date").removeClass("current-plan--selected");
+    $(".current-plan__renewal-date__lane-mate-yearly").addClass("current-plan--selected");
     $(".current-plan__summary").removeClass("current-plan--selected");
     $(".current-plan_lane-mate-yearly").addClass("current-plan--selected");
     $(".plan-option").removeClass("plan-option-selected");
@@ -16,6 +20,8 @@ $("#confirmLaneMateYearlyPlan").on( "click", function() {
 });
 
 $("#confirmUsmsYearlyPlan").on( "click", function() {
+    $(".current-plan__renewal-date").removeClass("current-plan--selected");
+    $(".current-plan__renewal-date__usms-yearly").addClass("current-plan--selected");
     $(".current-plan__summary").removeClass("current-plan--selected");
     $(".current-plan_usms-yearly").addClass("current-plan--selected");
     $(".plan-option").removeClass("plan-option-selected");
@@ -23,6 +29,8 @@ $("#confirmUsmsYearlyPlan").on( "click", function() {
 });
 
 $("#btnEditCreditCard").on("click", function() {
+    $(".cancel-auto-renew--confirmation").hide();
+    $(".cancel-auto-renew--form").hide();
     $(".payment-information--form").show();
 });
 
@@ -32,4 +40,23 @@ $("#btnEditCreditCardSubmit").on("click", function() {
 
 $("#btnEditCreditCardCancel").on("click", function() {
     $(".payment-information--form").hide();
+});
+
+
+
+$("#btnCancelAutoRenew").on("click", function() {
+    $(".cancel-auto-renew--confirmation").hide();
+    $(".payment-information--form").hide();
+    $(".cancel-auto-renew--form").show();
+});
+
+$("#btnYesCancelAutoRenew").on("click", function() {
+    $(".payment-information--form").hide();
+    $(".cancel-auto-renew--form").hide();
+    $(".cancel-auto-renew--confirmation").show();
+});
+
+$("#btnNoKeepAutoRenew").on("click", function() {
+    $(".payment-information--form").hide();
+    $(".cancel-auto-renew--form").hide();
 });
