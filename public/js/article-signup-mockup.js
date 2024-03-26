bce66ca9eeb5ca1b1fda9948470cbcd54877e276
J.Place <jplace@usmastersswimming.org>
2024-03-26 16:35:39 -0400

    no message

--- public/js/article-signup-mockup.js
+++
@@ -1,7 +0,0 @@
-$("#articleSignupContinue").on("click", function(){
-    $('.sign-up--inline').css('display','none');
-});
-
-$("#articleSignupDuplicateContinue").on("click", function(){
-    $('.sign-up--inline').css('display','none');
-});
