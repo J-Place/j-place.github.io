e9876bbf9cf5da8240bac1d1ce393a626dda432b
J.Place <jplace@usmastersswimming.org>
2024-04-19 16:24:03 -0400

    no message

---
+++ lanemate/js/donations.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+    $(".btn-donate--1").on('click', function() {
+        $("input[name='swimming-saves-lives']").val("30.00");
+    });
+    $(".btn-donate--2").on('click', function() {
+        $("input[name='swimming-saves-lives']").val("60.00");
+    });
+    $(".btn-donate--3").on('click', function() {
+        $("input[name='swimming-saves-lives']").val("120.00");
+    });
+    $(".btn-donate--4").on('click', function() {
+        $("input[name='swimming-saves-lives']").val("240.00");
+    });
+
+    $(".btn-donate").on('click', function() {
+        removeLineItem();
+        setCardValue();
+        renderLineItem();
+        getDonationValue();
+    });
+
+});
+
+var setInoputValue = function() {
+
+}
+
+var getDonationValue = function() {
+    var inputDonation = $("input[name='swimming-saves-lives']");
+    var donationVal = inputDonation.val();
+    console.log(donationVal);
+}
+
+var setCardValue = function() {
+    $('.total-donations.card__total--amount').text($('[name=swimming-saves-lives]').val());
+    $('.total-donations.card__total--amount').prepend('<span>$ </span>');
+    // $('.total-donations.card__total--amount').append('<span>.00</span>');
+}
+
+var renderLineItem = function() {
+    $(".payment-info__line-item--summary").append('<p class="payment-info__line-item payment-info__line-item--donation">Donation Total: <span class="payment-info__line-item--price"></span></p>');
+    updateLineItem();
+}
+
+var updateLineItem = function() {
+    $('.payment-info__line-item--donation .payment-info__line-item--price').text($('[name=swimming-saves-lives]').val());
+    $('.payment-info__line-item--donation .payment-info__line-item--price').prepend('<span>$ </span>');
+    // $('.payment-info__line-item--donation .payment-info__line-item--price').append('<span>.00</span>');
+}
+
+var removeLineItem = function () {
+    $(".payment-info__line-item--summary").find('.payment-info__line-item--donation').remove();
+}
+
+var formatCurrency = function(number) {
+    if(number == "") return;
+    number = parseFloat(number).toFixed(2);
+    var n = number.split('').reverse().join("");
+    console.log(n);
+    var n2 = n.replace(/\d\d\d(?!$)/g, "$&,");    
+    return '$' + n2.split('').reverse().join('');
+}
+
+// function formatCurrency(number){
+//     if(number == "") return;
+//     number = parseFloat(number).toFixed(2);
+//     var n = number.split('').reverse().join("");
+//     console.log(n);
+//     var n2 = n.replace(/\d\d\d(?!$)/g, "$&,");    
+//     return '$' + n2.split('').reverse().join('');
+// }
+
+
+$(document.body).on('change',function(){
+    
+
+//     var strokeSelectVal = $('#strokeSelect').val();
+//     // console.log(strokeSelectVal);
+//     var strokeNotSelected = strokeSelectVal === 'Select a stroke';
+//     if (strokeNotSelected === false) {
+//         // alert("One");
+//         $("#addVideoStrokeAnalysis").prop('disabled', false);
+//     } else {
+//         $("#addVideoStrokeAnalysis").prop('disabled', true);
+//         // alert("Two");
+//     }
+//     updateTotal();
+});
+
+// var updateTotal = function(){  
+//     var priceTextVideoStrokeAnalysis = $('payment-info__line-item--video-stroke-analysis');
+//     if (priceTextVideoStrokeAnalysis) {
+//         console.log("test 1");
+//     } else {
+//         console.log("test 2");
+//     }
+// };
---
+++ lanemate/js/product-option-video-stroke-analysis.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $("#addVideoStrokeAnalysis").on('click', function() {
+        $(".add-on-products .card__total--amount").text("$120.00");
+        $("#addVideoStrokeAnalysis").hide();
+        $("#removeVideoStrokeAnalysis").show();
+        $(".payment-info__line-item--summary").prepend('<p class="payment-info__line-item payment-info__line-item--video-stroke-analysis">Video Stroke Analysis: <span class="payment-info__line-item--price">$120.00</span></p>');
+    });
+    $("#removeVideoStrokeAnalysis").on('click', function() {
+        $("#strokeSelect").val('Select a stroke').change();
+        $(".add-on-products .card__total--amount").text("$__.__");
+        $("#addVideoStrokeAnalysis").prop("disabled",true);
+        $("#addVideoStrokeAnalysis").show();
+        $("#removeVideoStrokeAnalysis").hide();
+        $(".payment-info__line-item--summary").find('.payment-info__line-item--video-stroke-analysis').remove();
+    });
+});
+
+$(document.body).on('change','#strokeSelect',function(){
+    var strokeSelectVal = $('#strokeSelect').val();
+    // console.log(strokeSelectVal);
+    var strokeNotSelected = strokeSelectVal === 'Select a stroke';
+    if (strokeNotSelected === false) {
+        // alert("One");
+        $("#addVideoStrokeAnalysis").prop('disabled', false);
+    } else {
+        $("#addVideoStrokeAnalysis").prop('disabled', true);
+        // alert("Two");
+    }
+    updateTotal();
+});
+
+var updateTotal = function(){  
+    var priceTextVideoStrokeAnalysis = $('payment-info__line-item--video-stroke-analysis');
+    if (!priceTextVideoStrokeAnalysis) {
+        console.log("test 1");
+    } else {
+        console.log("test 2");
+    }
+};
