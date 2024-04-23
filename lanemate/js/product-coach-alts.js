5235745d9da271b3edec5ba4a0c0ed7eaf124a4b
J.Place <jplace@usmastersswimming.org>
2024-04-23 07:35:00 -0400

    working totals

---
+++ lanemate/js/product-coach-alts.js
@@ -0,0 +1,31 @@
+$(document).ready(function(updateProductCardTotal) {
+    $("#addCoachAlts").on('click', function() {
+        addCoachAlts(updateProductCardTotal);
+    });
+    $("#removeCoachAlts").on('click', function() {
+        removeCoachAlts(updateProductCardTotal);
+    });
+});
+
+// var coachAltsVal = 0;
+
+var addCoachAlts = function() {
+    $(".product-option.product-option_coach-alts").addClass('selected');
+    $("#addCoachAlts").hide();
+    $("#removeCoachAlts").show();    
+    updateProductCardTotal();
+}
+var removeCoachAlts = function() {
+    $(".product-option.product-option_coach-alts").removeClass('selected');
+    $("#addCoachAlts").show();
+    $("#removeCoachAlts").hide();
+    updateProductCardTotal();
+}
+var renderLineItemCoachAlts = function() {
+    removeLineItemCoachAlts();
+    $(".payment-info__line-item--summary").append('<p class="payment-info__line-item payment-info__line-item--coach-alts">Coach/ALTS Certification: <span class="payment-info__line-item--price"></span></p>');
+    $(".payment-info__line-item--coach-alts .payment-info__line-item--price").text('$ ' + coachAltsVal + '.00');
+}
+var removeLineItemCoachAlts = function() {
+    $(".payment-info__line-item--summary").find('.payment-info__line-item--coach-alts').remove();
+}
--- lanemate/js/review.js
+++ lanemate/js/review.js
@@ -1,31 +1,8 @@
 var updateTotalPayment = function(){
     var inputDonation = $("input[name='swimming-saves-lives']");
     var donationVal = inputDonation.val();
-    var donationValNum = Number(donationVal);
-    var videoValNum = Number(videoVal);
-    let addOnVideo = $(".payment-info__line-item--video-stroke-analysis");
-    let addOnDonation = $(".payment-info__line-item--donation");
-    var noVideoSelected = $("#addVideoStrokeAnalysis").prop('disabled');
-    if (noVideoSelected && addOnDonation.length !== 1) {
-        console.log("review 0");
-        $(".payment-info__line-item--total .payment-info__line-item--price").text("$__.__");
-    } else if (addOnVideo.length === !1 && addOnDonation.length !== 1) {
-        console.log("review 1");
-        $(".payment-info__line-item--total .payment-info__line-item--price").text("$__.__");
-    } else if (addOnVideo.length === 1 && addOnDonation.length === 1) {
-        console.log("review 2");
-        $(".payment-info__line-item--total .payment-info__line-item--price").text(videoValNum + donationValNum);
-        $(".payment-info__line-item--total .payment-info__line-item--price").prepend('<span>$ </span>');
-        $(".payment-info__line-item--total .payment-info__line-item--price").append('<span>.00</span>');
-    } else if (addOnDonation.length === 1 && addOnVideo.length !== 1) {
-        console.log("review 3");
-        $(".payment-info__line-item--total .payment-info__line-item--price").text(donationValNum);
-        $(".payment-info__line-item--total .payment-info__line-item--price").prepend('<span>$ </span>');
-        $(".payment-info__line-item--total .payment-info__line-item--price").append('<span>.00</span>');
-    } else if (addOnVideo.length === 1 && addOnDonation.length !== 1) {
-        console.log("review 4");
-        $(".payment-info__line-item--total .payment-info__line-item--price").text(videoValNum);
-        $(".payment-info__line-item--total .payment-info__line-item--price").prepend('<span>$ </span>');
-        $(".payment-info__line-item--total .payment-info__line-item--price").append('<span>.00</span>');
-    }
+    var donationNum = parseInt(donationVal);    
+    var totalPaymentVal = usmsPlusVal + videoVal + coachAltsVal + donationNum;
+    var totalPaymentNum = Number(totalPaymentVal);
+    $(".payment-info__line-item--total .payment-info__line-item--price").text('$ ' + totalPaymentNum + '.00');
 };
