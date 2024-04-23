9f23da6d08198fea9a987036c9126cd73e6f4a69
J.Place <jplace@usmastersswimming.org>
2024-04-23 09:24:40 -0400

    no message

---
+++ public/lanemate/js/products-usms.js
@@ -0,0 +1,111 @@
+$(document).ready(function() {
+    const mobile = window.matchMedia("screen and (max-width:1199px)").matches;
+    if (mobile) {
+        return;
+    } else {
+        // Find the tallest height
+        var tallestHeight = 0;
+        $('.product-option').each(function() {
+            var currentHeight = $(this).height();
+            if (currentHeight > tallestHeight) {
+            tallestHeight = currentHeight;
+            }
+        });
+        // Set the height
+        $('.product-option').height(tallestHeight);
+    }
+});
+var usmsPlusVal = 0;
+var videoVal = 0;
+var coachAltsVal = 0;
+var renderProductCardTotal = function() {
+    var cardTotalValProduct = usmsPlusVal + videoVal + coachAltsVal;
+    $(".add-on-products .card__total--amount").text("$ " + cardTotalValProduct + ".00");
+}
+var resetProductCardTotal = function() {
+    $(".add-on-products .card__total--amount").text("$__.__");
+}
+var updateProductCardTotal = function() {
+    console.log(videoVal);
+    console.log(coachAltsVal);
+    var usmsPlusSelected = $(".product-option_usms-plus.selected");
+    var videoSelected = $(".product-option_video-stroke-analysis.selected");
+    var coachAltsSelected = $(".product-option_coach-alts.selected");
+    if (coachAltsSelected.length !== 1 && usmsPlusSelected.length !== 1 && videoSelected.length !== 1) {
+        console.log("Coach Alts 1");
+        usmsPlusVal = 0;
+        videoVal = 0;
+        coachAltsVal = 0;
+        $(".add-on-products .card__total--amount").text('$__.__');
+        removeLineItemCoachAlts();
+        removeLineItemVideo();
+        updateTotalPayment();
+        // return;
+    } else if (coachAltsSelected.length !== 1 && usmsPlusSelected.length === 1 && videoSelected.length !== 1) {
+        console.log("Coach Alts 2");
+        usmsPlusVal = 179;
+        videoVal = 0;
+        coachAltsVal = 0;
+        removeLineItemCoachAlts();
+        renderProductCardTotal();
+        updateTotalPayment();
+        // return;
+    } else if (coachAltsSelected.length !== 1 && usmsPlusSelected.length !== 1 && videoSelected.length === 1) {
+        console.log("Coach Alts 3");
+        usmsPlusVal = 0;
+        videoVal = 120;
+        coachAltsVal = 0;
+        renderProductCardTotal();
+        renderLineItemVideo();
+        removeLineItemCoachAlts();
+        updateTotalPayment();
+        // return;
+    } else if (coachAltsSelected.length !== 1 && usmsPlusSelected.length === 1 && videoSelected.length === 1) {
+        console.log("Coach Alts 4");
+        usmsPlusVal = 179;
+        videoVal = 0;
+        coachAltsVal = 0;
+        renderProductCardTotal();
+        renderLineItemVideo();
+        removeLineItemCoachAlts();
+        updateTotalPayment();
+        // return;
+    } else if (coachAltsSelected.length === 1 && usmsPlusSelected.length !== 1 && videoSelected.length !== 1) {
+        console.log("Product Options 1");
+        usmsPlusVal = 0;
+        videoVal = 0;
+        coachAltsVal = 30;
+        renderProductCardTotal();
+        removeLineItemVideo();
+        renderLineItemCoachAlts();
+        updateTotalPayment();
+        // return;
+    } else if (coachAltsSelected.length === 1 && usmsPlusSelected.length === 1 && videoSelected.length === 1) {
+        console.log("Product Options 2");
+        usmsPlusVal = 179;
+        videoVal = 0;
+        coachAltsVal = 30;
+        // renderProductCardTotal();
+        renderLineItemVideo();
+        renderLineItemCoachAlts(); // <!---------------- ???
+        updateTotalPayment();
+    } else if (coachAltsSelected.length === 1 && usmsPlusSelected.length !== 1 && videoSelected.length === 1) {
+        console.log("Product Options 3");
+        usmsPlusVal = 0;
+        videoVal = 120;
+        coachAltsVal = 30;
+        renderProductCardTotal();
+        renderLineItemVideo();
+        renderLineItemCoachAlts();
+        updateTotalPayment();
+    } else if (coachAltsSelected.length === 1 && usmsPlusSelected.length === 1 && videoSelected.length !== 1) {
+        console.log("Product Options 4");
+        usmsPlusVal = 179;
+        videoVal = 0;
+        coachAltsVal = 30;
+        renderProductCardTotal();
+        removeLineItemVideo();
+        renderLineItemCoachAlts();
+        updateTotalPayment();
+    }
+};
