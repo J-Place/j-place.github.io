761902e369e82a962e6f1a99e3cc41d38e232519
J.Place <jplace@usmastersswimming.org>
2025-02-28 14:13:29 -0500

    no message

--- event/js/location-open-water.js
+++ event/js/location-open-water.js
@@ -1,26 +1,11 @@
 
 $(document).ready(function() {
 
-    // function showOpenWaterLocations() {
-    //     console.log("run Open Water Locaitons function")
-    //     $(".list-item-open-water").show();
-    //     scrollTopSection();
-    //     hideListControlChildren();
-    //     $(".list.locations").removeClass("edit-list");
-    //     $(".location-name .list-item__edit").show();
-    //     $(".location-name .list-item__save").hide();
-    //     $(".location-name .list-item__delete").hide();
-    // }
-    // showOpenWaterLocations();
-
-    // $(".list-venue-form .input-group.input-group--bulkhead").addClass("test");
-
     $('#locationType').val('Open Water').prop('disabled', true);
     $('#locationType').val('Open Water');
 
     $(".input-group--poolType").addClass("fade");
     $(".input-group--openWaterType").removeClass("fade");
     $(".input-group.input-group--bulkhead").hide();
-
 
 });
