205544b1cfb05420800f47791b206cd3332b90c0
J.Place <jplace@usmastersswimming.org>
2025-08-13 13:47:24 -0400

    no message

--- event/js/validation-calendar-to-sanction-open-water.js
+++ event/js/validation-calendar-to-sanction-open-water.js
@@ -4,7 +4,7 @@
 // $(".help-block--addSafetyCoordinator").addClass('has-error');
 // $(".help-block--SelectHostType").addClass('has-error');
 
-$(".help-block--agreeTerms").addClass("has-error");
+// $(".help-block--agreeTerms").addClass("has-error");
 $(".help-block--safety-plan").addClass("has-error");
 $(".help-block--course-map").addClass("has-error");
 // $(".").addClass("has-error");
