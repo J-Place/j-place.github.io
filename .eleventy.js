166516e7d485be284546ceb03714693c4accc263
J.Place <jplace@usmastersswimming.org>
2026-04-07 22:29:56 -0400

    Add passthrough for /public css paths

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,17 @@ module.exports = function(eleventyConfig) {
   // Copy public/ to _site/public/ untouched
   eleventyConfig.addPassthroughCopy("public");
 
+  // Also copy public subdirectory CSS folders to root so legacy
+  // Sergey pages with root-relative /xxx/css/ paths resolve correctly
+  eleventyConfig.addPassthroughCopy({ "public/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "public/search/css": "search/css" });
+  eleventyConfig.addPassthroughCopy({ "public/event/css": "event/css" });
+  eleventyConfig.addPassthroughCopy({ "public/lanemate/css": "lanemate/css" });
+  eleventyConfig.addPassthroughCopy({ "public/lanemate/css-components": "lanemate/css-components" });
+  eleventyConfig.addPassthroughCopy({ "public/registration/css": "registration/css" });
+  eleventyConfig.addPassthroughCopy({ "public/swimmer/css": "swimmer/css" });
+  eleventyConfig.addPassthroughCopy({ "public/forum-comments/css": "forum-comments/css" });
+
   // Course type abbreviation for small list view
   const courseTypeMap = { "OPEN WATER": "OW", "SHORT COURSE YARDS": "SCY", "SHORT COURSE METERS": "SCM", "LONG COURSE METERS": "LCM" };
   eleventyConfig.addFilter("courseTypeAbbr", val => courseTypeMap[val.toUpperCase()] || val);
